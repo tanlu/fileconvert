e1d20e35a0174ddfd21c6ae400d95af4fbc7891c
tanlu <394356286@qq.com>
2023-12-14 18:55:47 +0800

    修改页面等

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify,render_template,send_file
 import os
 import platform
-from pdf2docx import Converter
-import docx2pdf
 
+import docx2pdf
+import pythoncom
+from colorama import win32
+from flask import Flask, request, jsonify, render_template, send_file
+from pdf2docx import Converter
 
 app = Flask(__name__)
 
@@ -18,15 +20,18 @@ pdf1 = ".pdf"
 img1 = ".png"
 img2 = ".jpeg"
 
+
 # 首页
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+
 @app.route('/test', methods=['GET'])
 def test():
     return jsonify({'success': 'test'})
+
 
 # 文件上传
 @app.route('/upload', methods=['POST'])
@@ -55,28 +60,32 @@ def upload_file():
     return jsonify({"files": file_names, "file_path": file_path, 'result': True})
 
 
-def getSys():
-    system = platform.system()
-    if system == "Windows":
-        return 1
-    elif system == "Linux":
-        return 2
-    elif system == "Darwin":
-        return 3
-    else:
-        return 4
+# 文件转换入口，从这里进行分发
+@app.route('/fileconvert', methods=['GET'])
+def fileconvert():
+    file_path = request.args.get('filepath')
+    file_name = request.args.get("filename")
+    source_type = request.args.get('source_type')
+    end_type = request.args.get("end_type")
+    print(file_path)
+    print(file_name)
+    print(source_type)
+    print(end_type)
+    if ("."+source_type).lower() == pdf1.lower() and ("."+end_type).lower() == doc2.lower():  # pdf-docx
+        print("pdf-----docx  start...")
+        return convertPdf2Docx(file_path, file_name)
+    elif ("."+source_type).lower() == doc2.lower() and ("."+end_type).lower() == pdf1.lower():
+        return convert_docx_to_pdf()
+    return jsonify({"success": False, "msg": "暂不支持"})
+
 
 # pdf - docx
 @app.route('/convertPdf2Docx', methods=['GET'])
-def convertPdf2Docx():
-    # name = request.args.get('name', 'Guest')
-    file_path = request.args.get('filepath')
-    file_name = request.args.get("filename")
-
+def convertPdf2Docx(file_path, file_name):
     filePath = pdf2Docx(file_path, file_name)
-
-    re = {"file": filePath,"success":True}
+    re = {"file": filePath, "success": True}
     return jsonify(re);
+
 
 # pdf - doc
 @app.route('/convertPdf2Doc', methods=['GET'])
@@ -101,6 +110,7 @@ def pdf2Docx(file_path, file_name):
     cv.close()
     return file_path + filename + doc2
 
+
 #  pdf - doc
 def pdf2Doc(file_path, file_name):
     # convert pdf to docx
@@ -113,7 +123,7 @@ def pdf2Doc(file_path, file_name):
 
 # doc - pdf
 @app.route('/convertDocx2PDF', methods=['GET'])
-def convert_docx_to_pdf():
+def convert_docx_to_pdf(fi):
     file_path = request.args.get('filepath')
     file_name = request.args.get("filename")
     filename = os.path.splitext(os.path.basename(file_name))[0]
@@ -125,6 +135,7 @@ def convert_docx_to_pdf():
     # test.convert_to_pdf(file_path + file_name, file_path + filename + pdf1)
     re = {"filename": file_path + filename + pdf1}
     return jsonify(re)
+
 
 @app.route('/convertDoc2PDF', methods=['GET'])
 def convertDoc2PDF():
@@ -171,6 +182,7 @@ def download_file():
     filename = get_file_name_with_extension(file_path)
     return send_file(file_path, as_attachment=True, download_name=filename)
 
+
 # 获取带后缀的文件名
 def get_file_name_with_extension(file_path):
     # 提取文件名（带路径）
@@ -179,7 +191,22 @@ def get_file_name_with_extension(file_path):
     # 分离文件名和文件扩展名
     file_name, file_extension = os.path.splitext(file_name_with_path)
 
-    return file_name+file_extension
+    return file_name + file_extension
+
+
+# 获取当前系统
+def getSys():
+    system = platform.system()
+    if system == "Windows":
+        return 1
+    elif system == "Linux":
+        return 2
+    elif system == "Darwin":
+        return 3
+    else:
+        return 4
+
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", "80",False)
+    # app.run("0.0.0.0", "80",False)
+    app.run("localhost", "80", True)
---
+++ static/js/index.js
@@ -0,0 +1,5 @@
+// 获取文件后缀名
+function getFileExtension(filename){
+	var fileExtension = fileName.match(/\.(.+)$/)[1];
+	return fileExtension
+}
